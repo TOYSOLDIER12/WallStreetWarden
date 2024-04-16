9c50bc87da975ad194a57b52de9430c4d79c75a7
TOYSOLDIER12 <ilyasilyas2002@gmail.com>
2024-04-16 22:06:32 +0100

    2 push

--- python/arima.py
+++ python/arima.py
@@ -25,39 +25,20 @@ def get_csv(name):
 def arima(name):
     get_csv(name)
     df = pd.read_csv(name + ".csv")
-    plt.plot(df["Date"], df["Close"])
-    plt.xticks(np.arange(0, 1259, 200))
-    plt.title("TESLA stock price over time")
-    plt.xlabel("Time")
+
+    model = ARIMA(df['Close'], order=(4, 2, 0))
+    model_fit = model.fit()
+
+    forecast = model_fit.forecast(steps=10)
+
+    plt.plot(df["Date"], df["Close"], label='Historical Data')
+    plt.plot(range(len(df), len(df)+10), forecast, color='red', linestyle='--', label='Predicted Prices')
+    plt.title(f"{name} Stock Price Prediction for Next 10 Days")
+    plt.xlabel("Days")
     plt.ylabel("Price")
-    plt.show()
-
-    train_data, test_data = df[0:int(len(df)*0.7)], df[int(len(df)*0.7):]
-    training_data = train_data['Close'].values
-    test_data = test_data['Close'].values
-    history = [x for x in training_data]
-    model_predictions = []
-    N_test_observations = len(test_data)
-    for time_point in range(N_test_observations):
-        model = ARIMA(history, order=(4,2,0))
-        model_fit = model.fit()
-        output = model_fit.forecast()
-        yhat = output[0]
-        model_predictions.append(yhat)
-        true_test_value = test_data[time_point]
-        history.append(true_test_value)
-    MSE_error = mean_squared_error(test_data, model_predictions)
-    print('Testing Mean Squared Error is {}'.format(MSE_error))
-
-    test_set_range = df[int(len(df)*0.7):].index
-    plt.plot(test_set_range, model_predictions, color='blue', marker='o', linestyle='dashed', label='Predicted Price')
-    plt.plot(test_set_range, test_data, color='red', label='Actual Price')
-    plt.title('TESLA Prices Prediction')
-    plt.xlabel('Date')
-    plt.ylabel('Prices')
-    plt.xticks(np.arange(881, 1259, 50), df.Date[881:1259:50])
     plt.legend()
     plt.show()
+
 
 def main(stock_name):
     arima(stock_name)
