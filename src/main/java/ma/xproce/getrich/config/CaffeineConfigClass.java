d46d758fbddc09d8ba9fc7476a7ca61908f83421
TOYSOLDIER12 <ilyasilyas2002@gmail.com>
2024-06-27 16:35:14 +0100

    to show

---
+++ src/main/java/ma/xproce/getrich/config/CaffeineConfigClass.java
@@ -0,0 +1,24 @@
+package ma.xproce.getrich.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class CaffeineConfigClass {
+    @Bean
+    public Caffeine caffeineConfig() {
+        return Caffeine.newBuilder().expireAfterWrite(1, TimeUnit.DAYS);
+    }
+
+    @Bean
+    public CacheManager cacheManager(Caffeine caffeine) {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
+    }
+}
--- src/main/java/ma/xproce/getrich/service/Prediction.java
+++ src/main/java/ma/xproce/getrich/service/Prediction.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
-import java.util.Map;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,6 +14,7 @@ import java.util.Map;
 public class Prediction {
     private long id;
     private Date firstStockDate;
-    private Map<String,String> predictions;
+    private String Forecast;
+    private String forecastDate;
     private Enterprise enterprise;
 }
